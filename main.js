04887c77e7b154ac1b8344d40977dab9aa42f70b
SebastianoMV <sebastianovisco@gmail.com>
2022-04-07 17:39:20 +0200

    menù a comparsa con add e remove

---
+++ main.js
@@ -0,0 +1,21 @@
+const activeMenuH = document.querySelector('.header-right > a');
+console.log(activeMenuH);
+
+const menuHamburger = document.querySelector('.hamburger-menu');
+console.log(menuHamburger);
+
+const activeClose = document.querySelector('.close');
+
+activeMenuH.addEventListener('click', function(){
+  menuHamburger.classList.add('active');
+})
+
+activeClose.addEventListener('click', function(){
+  menuHamburger.classList.remove('active');
+})
+
+
+
+
+  
+  
